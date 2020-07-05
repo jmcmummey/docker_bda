ecdd4c8626e164b8143372356f2a0792856e78db
Richard Torkar <richard.torkar@gmail.com>
2020-07-05 10:02:47 +0200

    affective states repo added

--- scripts/install_repl.sh
+++ scripts/install_repl.sh
@@ -2,5 +2,5 @@
 set -e
 
 # If you want to make sure that the Docker image downloads a certain repository
-# mkdir -p /home/rstudio/development \
-#    && git clone https://github.com/torkar/affective_states.git
+mkdir -p /home/rstudio/development \
+    && git clone https://github.com/torkar/affective_states.git
