396726d1dede5312bb3fb951c3fa1330d3464956
Richard Torkar <richard.torkar@gmail.com>
2020-07-05 13:47:58 +0200

    Update install_repl.sh

--- scripts/install_repl.sh
+++ scripts/install_repl.sh
@@ -4,4 +4,5 @@ set -e
 # If you want to make sure that the Docker image downloads a certain repository
 mkdir -p /home/rstudio/development \
     && cd /home/rstudio/development \
-    && git clone https://github.com/torkar/affective_states.git
+    && git clone https://github.com/torkar/affective_states.git \
+    && chown -R rstudio:rstudio /home/rstudio/development
