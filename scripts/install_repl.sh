d254cd13e4647c5f0ce9a26c402bb066a4a6b3ac
Richard Torkar <richard.torkar@gmail.com>
2020-07-10 08:46:31 +0200

    Added replication package

--- scripts/install_repl.sh
+++ scripts/install_repl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-# If you want to make sure that the Docker image downloads a certain repository
-# mkdir -p /home/rstudio/development \
-#    && git clone https://github.com/torkar/affective_states.git
+# Store the replication package inside the Docker image
+mkdir -p /home/rstudio/development \
+    && cd /home/rstudio/development \
+    && git clone https://github.com/torkar/feature-selection-RBS.git \
+    && chown -R rstudio:rstudio /home/rstudio/development
