18c9c85d790b0b063c19957b438abc34e439a273
Richard Torkar <richard.torkar@gmail.com>
2020-07-10 13:44:34 +0200

    Update install_repl.sh

--- scripts/install_repl.sh
+++ scripts/install_repl.sh
@@ -4,5 +4,5 @@ set -e
 # Store the replication package inside the Docker image
 mkdir -p /home/rstudio/development \
     && cd /home/rstudio/development \
-    && git clone https://github.com/torkar/feature-selection-RBS.git \
+    && git clone --depth 1 https://github.com/torkar/feature-selection-RBS.git \
     && chown -R rstudio:rstudio /home/rstudio/development
